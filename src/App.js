6596b05c215d83c658bd31921b3c93b94a0e131a
iamniama <plectrumonthespectrum@gmail.com>
2021-02-24 14:10:53 -0700

    added subtle, random backgroundColor shifts each time user interacts with a control

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ function App() {
         {word: "bluejay", color: "red"},
         {word: "Orinoco", color: "green"},
         {word: "politico", color: "whitesmoke"},
-        {word: "night", color: "lightpurple"},
-        {word: "purple", color: "goldenrod"},
+        {word: "night", color: "goldenrod"},
+        {word: "purple", color: "yellow"},
         {word: "pinto", color: "skyblue"},
         {word: "19", color: "darkred"},
-        {word: "Allegheny", color: "chocolate"}]
+        {word: "Allegheny", color: "chocolate"},
+        {word: "Mercedes", color: "darkslategrey"},
+        {word: "WILL", color: "darkred"},
+        {word: "dreams", color: "whitesmoke"},
+        {word: "faith", color: "black"},
+        {word: "blue", color: "darkred"}]
   return (
     <div className="App">
-      <header className="App-header">
+
           <List items={dataItems} />
-      </header>
+
     </div>
   );
 }
--- src/list.js
+++ src/list.js
@@ -1,33 +1,66 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class List extends React.Component {
     constructor(props){
         super()
         this.state = {
             itemArray: props.items,
-            currentIndex: 0
+            currentIndex: 0,
+            red: 91,
+            green: 109,
+            blue: 122
         }
     }
 
-    decIndex(e){
-        if (this.state.currentIndex > 0){
+    //colorFiddle is called by all eventListeners (need to look into bind)
+    //and subtly adjusts the backgroundColor of the page
+    colorFiddle() {
+        //randomly choose red, green, or blue for the rgb, and assign that color name to a variable
+        //so that we can use it to change state later
+        const colors = ["red", "green", "blue"]
+        let changeColor = colors[Math.floor(Math.random() * colors.length)]
+        //get the current value of the randomly selected color from state
+        let ccVal = this.state[changeColor]
+        //flip a coin, if heads, add a random 1-5 to the value,
+        //tails, subtract the same
+        switch (Math.floor(Math.random() * 2)){
+            case 0:
+                ccVal += Math.floor(Math.random() * 5)
+                break;
+            case 1:
+                ccVal -= Math.floor(Math.random() * 5)
+                break;
+            default:
+                break;
+        }
+        //update the selected value in state
         this.setState({
-            currentIndex: this.state.currentIndex - 1
+            [changeColor]: ccVal
         })
-        }else{
+    }
+
+    decIndex(e) {
+        this.colorFiddle()
+        if (this.state.currentIndex > 0) {
+            this.setState({
+                currentIndex: this.state.currentIndex - 1
+            })
+        } else {
             this.setState({
                 currentIndex: this.state.itemArray.length - 1
             })
         }
     }
 
-    incIndex(e){
-        if (this.state.currentIndex < this.state.itemArray.length -1){
-        this.setState({
-            currentIndex: this.state.currentIndex + 1
-        })
-        }else {
+    incIndex(e) {
+        this.colorFiddle()
+        if (this.state.currentIndex < this.state.itemArray.length - 1) {
+            this.setState({
+                currentIndex: this.state.currentIndex + 1
+            })
+        } else {
             this.setState({
                 currentIndex: 0
             })
@@ -39,15 +72,17 @@ class List extends React.Component {
         let item = this.state.itemArray[this.state.currentIndex]
         //let item = itemList[this.state.currentIndex].word
         return(
+            <header className="App-header" style={{backgroundColor: `rgb(${this.state.red}, ${this.state.green}, ${this.state.blue})`}}>
             <div className="list-wrapper">
 
-                <h1 className={item.color}>{item.word}</h1>
+                <h1 style={{color: `${item.color}`}}>{item.word}</h1>
                 <div className="nav-row">
-                    <Button variant="outline-primary" onClick={(e) => this.decIndex(e)}>Left</Button>
-                    <Button variant="outline-primary" onClick={(e) => this.incIndex(e)}>Right</Button>
+                    <Button variant="outline-primary" type="button" onClick={(e) => this.decIndex(e)}>Left</Button>
+                    <Button variant="outline-primary" type="button" onClick={(e) => this.incIndex(e)}>Right</Button>
 
                 </div>
             </div>
+            </header>
         )
 
 
